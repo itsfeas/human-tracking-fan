a4f3dee2b9164e9011fa08a855ccf5bf7823bfde
itsfeas <amf.fiaz@gmail.com>
2021-06-12 15:53:42 -0600

    added hog filter

--- human-tracking/HOG.py
+++ human-tracking/HOG.py
@@ -1,10 +1,7 @@
-
 import numpy as np
 from scipy import ndimage as nd
-from skimage import img_as_float
+from skimage.feature import hog
 from skimage.filters import roberts, sobel, scharr, prewitt, threshold_otsu, threshold_multiotsu
-from skimage.feature import peak_local_max
-from skimage.exposure import rescale_intensity
 import skimage.segmentation
 
 from matplotlib import pyplot as plt
@@ -13,11 +10,29 @@ import math
 
 DEBUG = True
 
-filepath = "./Testing/"
+filepath = "./frames/"
+
+def resize(img):
+	height, width, channels = img.shape
+	dimensions = (width//4, height//4)
+	img = cv2.resize(img, dimensions, interpolation = cv2.INTER_AREA)
+	return img
+
+def hog_filter(img, orient, radius):
+	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	fd, hog_image = hog(img_gray, orientations=8, pixels_per_cell=(radius, radius), cells_per_block=(1, 1), visualize=True)
+	return hog_image
 
 if __name__ == '__main__' and DEBUG:
-	# file1 = "frame77.jpg"
-	# file2 = "frame79.jpg"
-	# file3 = "frame81.jpg"
-	# file4 = "frame83.jpg"
-	# file5 = "frame65.jpg"
+	file1 = "frame0.png"
+	file2 = "frame1.png"
+	file3 = "frame2.png"
+
+	img = cv2.imread(filepath+file1)
+	img = resize(img)
+	cv2.imshow("img", img)
+	output_img = hog_filter(img, 8, 10)
+	cv2.imshow("hog_filter", output_img)
+	
+	cv2.waitKey(0)
+	cv2.destroyAllWindows
--- human-tracking/frame_extraction.py
+++ human-tracking/frame_extraction.py
@@ -1,9 +1,10 @@
+import cv2
 from os import walk
 from os import remove
 
-FRAME = 1150
-VIDEOPATH = "./Videos/"
-FRAMEPATH = "./Frames/"
+FRAME = 180
+VIDEOPATH = "./videos/"
+FRAMEPATH = "./frames/"
 
 def list_files(mypath):
 	f = []
@@ -18,7 +19,7 @@ file_list = list_files(VIDEOPATH)
 i=0
 for filename in file_list:
 	vid = cv2.VideoCapture(VIDEOPATH + filename)
-	vid.set(cv2.CAP_PROP_POS_FRAMES, frame)
+	vid.set(cv2.CAP_PROP_POS_FRAMES, FRAME)
 	success, image = vid.read()
 	cv2.imwrite(FRAMEPATH + "frame" + str(i) + ".png", image)
 	i+=1
