40a2085fa993a35daf7c0e27aa417a41ce85596f
itsfeas <amf.fiaz@gmail.com>
2021-09-30 21:36:09 -0600

    added functionality to left and right buttons in web app

--- web-app/static/js/manual_controls.js
+++ web-app/static/js/manual_controls.js
@@ -21,3 +21,37 @@ function engageSwitch(impButton) {
         })
 
 }
+
+function turnLeft() {
+    fetch('/left', {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(impButton.textContent),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    })
+        .then(function (response) {
+            // impButton.textContent = "Might've Engaged"
+            return response.json();
+        })
+    
+}
+
+function turnRight() {
+    fetch('/right', {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(impButton.textContent),
+        cache: "no-cache",
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    })
+        .then(function (response) {
+            // impButton.textContent = "Might've Engaged"
+            return response.json();
+        })
+
+}
